5bed8cd25e097d1e771a894262680e20458a18db
Elkwizard <bananamanthefourth@gmail.com>
2026-06-28 21:46:22 -0700

    renamed logging functions

--- util/debug.hpp
+++ util/debug.hpp
@@ -20,7 +20,7 @@ namespace util {
 		std::string formatLineNumber(const char* file, int line);
 		
 		template <int N>
-		inline void printValue(const char* expr, const char (&value)[N], bool lock) {
+		inline void logValue(const char* expr, const char (&value)[N], bool lock) {
 			if (lock) coutMutex.lock();
 			std::cout << value << std::endl;
 			std::cout.flush();
@@ -28,7 +28,7 @@ namespace util {
 		}
 		
 		template <int N>
-		inline void printValue(const char* expr, const wchar_t (&value)[N], bool lock) {
+		inline void logValue(const char* expr, const wchar_t (&value)[N], bool lock) {
 			if (lock) coutMutex.lock();
 			std::wcout << value << std::endl;
 			std::wcout.flush();
@@ -36,7 +36,7 @@ namespace util {
 		}
 
 		template <typename T>
-		inline void printValue(const char* expr, const T& value, bool lock) {
+		inline void logValue(const char* expr, const T& value, bool lock) {
 			if (lock) coutMutex.lock();
 			std::cout << expr << ": " << value << std::endl;
 			std::cout.flush();
@@ -44,7 +44,7 @@ namespace util {
 		}
 
 		template <>
-		inline void printValue(const char* expr, const std::wstring& value, bool lock) {
+		inline void logValue(const char* expr, const std::wstring& value, bool lock) {
 			if (lock) coutMutex.lock();
 			std::cout << expr << ": ";
 			std::cout.flush();
@@ -56,39 +56,39 @@ namespace util {
 		}
 		
 		template <typename T>
-		inline void uniquePrintValue(const char* expr, const T& value) {
+		inline void uniqueLogValue(const char* expr, const T& value) {
 			int maxValue = 100;
 			std::string str = std::to_string(abs(rand() % maxValue));
 			str = std::string(std::to_string(maxValue - 1).size() - str.length(), '0') + str;
 			std::unique_lock guard { coutMutex };
 			std::cout << str << " | ";
 			std::cout.flush();
-			printValue(expr, value, false);
+			logValue(expr, value, false);
 		}
 	}
 }
 
-// #define PRINT_LINE_NUMBERS
+// #define LOG_LINE_NUMBERS
 
-#ifdef PRINT_LINE_NUMBERS
+#ifdef LOG_LINE_NUMBERS
 
 #define FORMATTED_LN util::debug::formatLineNumber(__FILE__, __LINE__)
-#define print(v) util::debug::printValue((FORMATTED_LN + #v).c_str(), (v), true)
-#define uprint(v) util::debug::uniquePrintValue((FORMATTED_LN + #v).c_str(), (v));
-#define rprint(v) std::cout << FORMATTED_LN << v << std::endl
+#define log(v) util::debug::logValue((FORMATTED_LN + #v).c_str(), (v), true)
+#define ulog(v) util::debug::uniqueLogValue((FORMATTED_LN + #v).c_str(), (v));
+#define rlog(v) std::cout << FORMATTED_LN << v << std::endl
 
 #else
 
-#define print(v) util::debug::printValue(#v, (v), true)
-#define uprint(v) util::debug::uniquePrintValue(#v, (v));
-#define rprint(v) std::cout << v << std::endl
+#define log(v) util::debug::logValue(#v, (v), true)
+#define ulog(v) util::debug::uniqueLogValue(#v, (v));
+#define rlog(v) std::cout << v << std::endl
 
 #endif
 
 #define alert(v) MessageBoxA(NULL, std::to_string(v).c_str(), "Alert", MB_OK)
-#define palert(v) do { print(v); alert(v); } while (false)
+#define palert(v) do { log(v); alert(v); } while (false)
 
-#ifdef PRINT_CONSTRUCTION
+#ifdef LOG_CONSTRUCTION
 
 #define CONSTRUCT(type) std::cout << "Constructed " #type << std::endl
 #define DESTRUCT(type) std::cout << "Destructed " #type << std::endl
