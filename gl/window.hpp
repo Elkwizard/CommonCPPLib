d0dbb0770db1cb2c6dfb59e0c2deebe94e039cc7
Elkwizard <bananamanthefourth@gmail.com>
2026-07-06 13:02:53 -0700

    added additional vulkan #define

--- gl/window.hpp
+++ gl/window.hpp
@@ -12,6 +12,7 @@
 #include <unordered_map>
 
 #ifdef COMMON_CPP_VULKAN
+#define VULKAN_HPP_HANDLE_ERROR_OUT_OF_DATE_AS_SUCCESS
 #define VK_USE_PLATFORM_WIN32_KHR
 #define VULKAN_HPP_NO_STRUCT_CONSTRUCTORS
 #include <vulkan/vulkan_raii.hpp>
